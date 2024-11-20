092bc8dd0800315c15e2692dc947b917ab2f6990
imaddine1 <hh.imad2016@gmail.com>
2024-11-20 21:23:02 +0100

    structure the folders

---
+++ part2/scripts/script.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#install k3s
+curl -sfL https://get.k3s.io | sh -s - server --node-ip 192.168.56.110
+
+
+#in the subject they use a shortuct for kubectl 
+echo "alias k='kubectl'" >> /home/vagrant/.bashrc
+
+#run the deployment manifest files to get our pods working
+cd /tmp/deployment
+kubectl apply -f .
+
+# wait a little bit , to make sure the pods are running
+sleep 5
+
+#run the services manifest files so we can access to the working pods
+cd /tmp/services
+kubectl apply -f .
+
+sleep 3
+
+# now i need to run ingress to route these running services
+cd /tmp/ingress
+kubectl apply -f ingress-config.yaml
+
+
+############ i want to run the borwser but i took a long time so i skip it #############
+
+# installing GUI
+sudo apt update && sudo apt upgrade -y
+sudo apt install xfce4 xfce4-goodies -y
+
+# installing firefox
+sudo apt install firefox-esr -y
+
+
+
+####### ALL THE NEDDED OBJECT RUNNING NOW #############
+####### NB: you can use the kube cli to run those object without creating yaml file , if you want ###############
