17147b6f550a34ffe2504d4e01d489f387506077
imaddine1 <hh.imad2016@gmail.com>
2025-01-10 14:56:47 +0100

    modify p2 && add bonus

--- p2/scripts/script.sh
+++ p2/scripts/script.sh
@@ -27,12 +27,12 @@ kubectl apply -f ingress-config.yaml
 
 ############ i want to run the borwser but i took a long time so i skip it #############
 
-# installing GUI
-sudo apt update && sudo apt upgrade -y
-sudo apt install xfce4 xfce4-goodies -y
+# # installing GUI
+# sudo apt update && sudo apt upgrade -y
+# sudo apt install xfce4 xfce4-goodies -y
 
-# installing firefox
-sudo apt install firefox-esr -y
+# # installing firefox
+# sudo apt install firefox-esr -y
 
 
 
